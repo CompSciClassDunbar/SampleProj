e1afd605b5223128b9c2c947e611d04cb8137be0
kbstacy4444 <31349557+kbstacy4444@users.noreply.github.com>
2017-08-25 10:31:34 -0400

    Add files via upload

--- LittleJimmyFink.java
+++ LittleJimmyFink.java
@@ -1,57 +1,72 @@
 import java.util.*;
+
 public class LittleJimmyFink {
-
-	public static void main(String[] args) {
-		Scanner fink = new Scanner(System.in);
-		String name; int lnum, bnum; double total, falsetotal,ldiscount = 0, bdiscount = 0, tdiscount = 0; boolean finished = true; double suptotal = 0;
-		while(finished){
-		System.out.println("Name?");
-		name = fink.next();
+	public static void main(String[] args){
+		Scanner thing = new Scanner(System.in);
+		String name = null;		
+		short lemonade = 0;
+		short brownies = 0;
+		double total = 0;
+		int totalCust = 0;
 		
-		System.out.println("Number of lemonades");
-		lnum = fink.nextInt();
-		System.out.println("Number of brownies");
-		bnum = fink.nextInt();
+		while(true){
+			System.out.println("Oh look! A new customer!");
+			System.out.println("Bro, what's your name?");
+			name = thing.next();
 		
-		double discL, discB;
-		falsetotal = (lnum * 0.79) + (bnum * 1.29);
-		if(lnum >= 5 && bnum < 5){
-			discL = (0.79 * lnum)- (0.79 * lnum)*(0.05);
-			total = discL + (bnum * 1.29);
-			ldiscount = 5;
-			System.out.println("You get a 5% discount on your lemonades");
+			System.out.println("how many of them tasty brownies you want?");
+			brownies = thing.nextShort();
+		
+			System.out.println("how many cups of that hella refreshing lemonade do you want?");
+			lemonade = thing.nextShort();
+		
+			double cost = 0;
+			double newCost = 0;
 			
-		}
-		else if(lnum<5 && bnum >= 5){
-			discB = (1.29 * bnum)- (1.29 * bnum)*(0.1);
-			total = discB + (lnum * 0.79);
-			bdiscount = 10;
-			System.out.println("You get a 10% discount on your brownies");
-		}
-		else if(lnum >= 5 && bnum >= 5){
-			total = falsetotal - (falsetotal * .22);
-			tdiscount = 22;
-			System.out.println("Your obtained a 22% discount on your total pricing");
-		}
-		else{
-			total = falsetotal;
-			System.out.println("wow what a loser");
-		}
-		total += total + (.06*total);
-		
-		System.out.println(name+" purchased:");
-		System.out.println(lnum+" lemonades");
-		System.out.println("and "+bnum+" brownies");
-		System.out.println("with a "+ldiscount+"% discount on your lemonade");
-		System.out.println("and a "+bdiscount+"% discount on your brownies");
-		System.out.println("Your total cost is "+total+" with a 6% sales tax and a "+tdiscount+"% total discount");
-		
-		System.out.println("Are there any more people? (true for yes false for no)");
-		finished = fink.nextBoolean();
-		suptotal += total;
-		}
-		System.out.println("The total amount of money made today is: "+suptotal);
+			if(lemonade>5 && brownies<5){
+				cost = ((0.79*lemonade)*.95) + (1.29*brownies);
+				newCost = (cost*.06)+cost;
+			}
+			else if(lemonade<5 && brownies>5){
+				cost = (0.79*lemonade) + ((1.29*brownies)*.9);
+				newCost = (cost*.06)+cost;
+			}
+			else if(lemonade>5 && brownies>5){
+				cost = ((0.79*lemonade) + (1.29*brownies))*.78;
+				newCost = (cost*.06)+cost;
+			}
+			else{
+				cost = (0.79*lemonade) + (1.29*brownies);
+				newCost = (cost*.06)+cost;
+			}
+			
+			
+			System.out.println("Customer: "+name);
+			System.out.println("Number of brownies: "+brownies);
+			System.out.println("Cups of Lemonade: "+lemonade);
+			if(lemonade>=5 && brownies<5){
+				System.out.println("5% Discount");
+			}
+			else if(lemonade<5 && brownies>=5){
+				System.out.println("10% Discount");
+			}
+			else if(lemonade>=5 && brownies>=5){
+				System.out.println("22% Discount");
+			}
+			else{
+				System.out.println("No Discount");
+			}
+			System.out.println("Sales Tax: $"+(cost*.6));
+			System.out.println("Total Cost: "+newCost);
+			System.out.println("Have a good day, "+name+"! Don't forget to come back to Little Johnny Fink's Snack Stand!");
+			
+			total += newCost;
+			totalCust += 1;
 
+			System.out.println("****************");
+			System.out.println("The total so far is "+totalCust+" Customer(s) so far. I've made $"+total+" today");
+			System.out.println("****************");
+		}	
+		
 	}
-
 }
