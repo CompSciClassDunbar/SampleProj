0a1b0ac34427c620af37e8ae30b0b77d467eaccb
sashfaq644 <31349504+sashfaq644@users.noreply.github.com>
2017-08-25 10:29:49 -0400

    Add files via upload

--- Lemonade.java
+++ Lemonade.java
@@ -1,65 +1,75 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class Lemonade {
 
 	public static void main(String[] args) {
-		Scanner reader = new Scanner (System.in);
-		double finalCost=0;
-		boolean another=true;
-		while (another==true){
+		Scanner Billy = new Scanner(System.in);
+		
+		//i didn't do step 3
+		
+		String name;
+		String discount="0%";
+		
+		float numLem;
+		float numBrow;
+		
 
-
-			String[] want=new String[5];
-			int[] newWant=new int[2];
-
-			String[] product={"lemonades", "brownies"};
-			double lem=0.79;
-			double brownie=1.29;
-			double tCost=0;
-
-			String name="";
-			System.out.println("Please enter your name");
-			want[0]=reader.nextLine();
-
-			for (int i=1; i<3; i++){
-				System.out.println("Type in the number of "+product[i-1]+" you want to purchase");
-				want[i]=reader.nextLine();
-				int temp=Integer.parseInt(want[i]);
-				newWant[i-1]=temp;
+		
+		boolean fiveB= false;
+		boolean fiveL= false;
+		
+		double fCost=0;
+		boolean customer= true;
+		
+		String newP="";
+		
+	
+			
+			
+			System.out.println("What is name of the customer?");
+			name= Billy.nextLine();
+			System.out.println("How many lemonades do you want?");
+			numLem= Billy.nextInt();
+			System.out.println("How many brownies do you want?");
+			numBrow= Billy.nextInt();
+			
+		
+			if(numBrow>=5)
+				fiveB= true;
+			if(numLem>=5)
+				fiveL= true;
+			
+			if(fiveB==true && fiveL==true){
+				fCost=.78 * (1.29 * numBrow + .79 * numLem);
+				discount="22%";
 			}
-			double discount=0;
-			double lemCost=lem*newWant[0];
-			double brownCost=brownie*newWant[1];
-			boolean Tdiscount=false; boolean Ldiscount=false; boolean Bdiscount=false;
-			if (newWant[0]>=5 && newWant[1]>=5){
-				tCost=(lemCost+brownCost)*0.78;
-				Tdiscount=true; discount=(lemCost+brownCost)*0.22;
+			if(fiveB==true && fiveL==false){
+				fCost= .9 *(1.29 * numBrow) + .79 * numLem;
+				discount="10%";
 			}
-			else if (newWant[1]>=5){
-				brownCost=brownCost*0.9;
-				tCost=lemCost+brownCost;
-				Bdiscount=true; discount=brownCost*0.1;
+			if(fiveB==false && fiveL==true){
+				fCost= 1.29 * numBrow + .95 *(.79 * numLem);
+				discount="05%";
 			}
-			else if (newWant[0]>=5){
-				lemCost=lemCost*0.95;
-				tCost=lemCost+brownCost;
-				Ldiscount=true; discount=lemCost*0.05;
-			}
-			double tax=tCost*0.06;
-			System.out.println(want[0]);
-			System.out.println("You purchased "+want[1]+" lemonades for a total of $"+lemCost);
-			System.out.println("You purchased "+want[2]+" brownies for a total of $"+brownCost);
-			System.out.println("Your subtotal is $"+tCost); System.out.println("Your tax is $"+tax);
-			tCost=tCost+tax;
-			System.out.println("Your total cost is: $"+tCost);
-			if (discount!=0)
-				System.out.println("Discount received: $"+discount);
-			System.out.println("Is there another customer? If yes, please type yes.");
-			String cust=reader.nextLine();
-			if (cust=="no" || cust=="No")
-				another=false;
-			finalCost+=tCost;
-		}	
-		System.out.println(finalCost);
+			if(fiveB==false && fiveL==false)
+				fCost= 1.29 * numBrow + .79 * numLem;
+			total+= fCost;
+			
+			fCost+= fCost*.06;
+			
+			System.out.println("Recipt for customer: "+ name);
+			System.out.println(numBrow + " Brownies: $" + 1.29 * numBrow );
+			System.out.println(numLem + " Lemonades: $" + .79 * numLem);
+			System.out.println("You have earned a discount of: " + discount);
+			System.out.println("Tax: 06%");
+			System.out.println("Total cost with dicount and tax: $" + fCost);
+			System.out.println("Have a nice day!");
+			
+			
+			
+			
+			
+		
+		
 	}
+
 }
