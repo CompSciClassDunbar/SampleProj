78657c5b9d8e20a27cdce9d31116526c16cc4515
Argonath90 <31349625+Argonath90@users.noreply.github.com>
2017-08-25 10:29:20 -0400

    Add files via upload

--- lemonade.java
+++ lemonade.java
@@ -1,88 +1,53 @@
 import java.util.*;
+
 public class lemonade {
+	static double lemC=.79, broC=1.29;//setting cost without discounts
 
 	public static void main(String[] args) {
-		//variables
-		double lemCost= 0.79;
-		double brownCost= 1.29;
-		Scanner scan= new Scanner(System.in);
-		int numLem=0;
-		int numBrown=0;
-		double costBrown=0;
-		double costLem=0;
-		double salesTax;
-		double total=0;
-		double savings=0;
-		double totalSales=0;
-		String name;
-		boolean brownDis=false;
-		boolean lemDis=false;
-		boolean bigDis=false;
-		boolean isNext=true;
+		ArrayList<String> peeps=new ArrayList<String>();
+		String nm;									//making
+		short numL, numB;								//cool
+		double total;									//variables
+		Scanner scanny=new Scanner(System.in);			//oh
+		boolean stillCustomers;							//yeah
 		
-		//talk to the person
-		
-		while(isNext){
-			System.out.println("What is your name?");
-			name= scan.next();
-			System.out.println("How many brownies would you like to buy?");
-			numBrown= scan.nextInt();
-			System.out.println("How many cups of lemonade would you like to buy?");
-			numLem= scan.nextInt();
-		
-			//Check discounts
-			if(numBrown>4 && numLem>4)
-				bigDis=true;
-			else if(numBrown>4)
-				brownDis= true;
-			else if(numLem>4)
-				lemDis=true;
-		
-			//Math
-			costBrown= numBrown*brownCost;
-			if(brownDis){
-				savings= costBrown*.1;
-				costBrown= costBrown-costBrown*.1;
-			}
+			System.out.print("Enter name:");
+			nm=scanny.nextLine();				//put scanny to work
+			System.out.println("How many lemonades would you like?");
+			numL=scanny.nextShort();
+			if(numL>5)									//applies discount
+				lemC*=.95;
+			System.out.println("And how many brownies?");
+			numB=scanny.nextShort();
+			if(numB>5)									//applies discount
+				broC*=.9;
+			scanny.close();								//closes scanner
 			
-			costLem= numLem*lemCost;
-			if(lemDis){
-				savings= costLem*.05;
-				costLem= costLem-costLem*.05;
-			}
+			total=lemC*numL+broC*numB;					//calc total
+			if(numL>5 && numB>5)						//applies total discount
+				total*=.78;
+			double totTax=total*1.06;					//adds tax
 		
-			total= costLem + costBrown;
-			if(bigDis){
-				savings= total*.22;
-				total= total-total*.22;
-			}
-			salesTax=total*.06;
-			
-			totalSales= totalSales+ total+ salesTax;
-		
-			//tell them stuff
-			System.out.println("Hey, "+name);
-			System.out.println("You've ordered "+ numBrown+" brownie(s).");
-			System.out.println("You've ordered "+ numLem+" cup(s) of lemonade.");
-		
-			System.out.println("You've spent $"+ costBrown+" on brownies before any discounts.");
-			System.out.println("You've spent $"+ costLem+" on lemoande before any discounts.");
-		
-			if(brownDis)
-				System.out.println("You've qualified for the brownie discount!! You saved $"+savings+" on brownies");
-			if(lemDis)
-				System.out.println("You've qualified for the lemonade discount!! You saved $"+savings+" on lemonade");
-			if(bigDis)
-				System.out.println("You've qualified for the bulk buyer's discount!! You saved $"+savings+" on your total");
-		
-			System.out.println("A sales tax of $"+salesTax+" will be added");
-			System.out.println("In total you've spent $"+(total+salesTax));
-			System.out.println("Thanks for shopping at Eli's Stand, our total sales today have been $"+totalSales);
-			
-			System.out.println("Is there a customer next in line?");
-			isNext= scan.nextBoolean();
-		}
-		scan.close();
+		//making a nice formatted receipt
+		System.out.println();
+		System.out.println();
+		System.out.println("Little Jimmy's Lemonade \n");
+		System.out.println("Transaction "+(int)(Math.random()*10000)+1);
+		System.out.println("8/23/17 \n");
+		System.out.println(nm);
+		System.out.println("Lemonade\t"+numL+"\t"+lemC*numL);
+		if(numL>5)
+			System.out.println("\tdisc 5%\t\t-"+(lemC*.05)*numL);
+		System.out.println("Brownie\t\t"+numB+"\t"+broC*numB);
+		if(numB>5)
+			System.out.println("\tdisc 5%\t\t-"+(broC*.05)*numB);
+		System.out.println();
+		System.out.println("\tSubtotal:\t"+total);
+		if(numL>5 && numB>5)
+			System.out.println("\t    disc 22%    -"+total*.22);
+		System.out.println("\tTax:\t\t"+total*.06);
+		System.out.println("\tTotal:\t\t"+totTax);
+		System.out.println();
+		System.out.println("Get away from me now");
 	}
-
 }
