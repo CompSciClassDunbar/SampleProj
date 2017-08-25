91d50b78dbed629caf844c74145d46bd5e15cabe
Jacob Caudill <31349503+bobster6785@users.noreply.github.com>
2017-08-25 10:29:44 -0400

    Add files via upload

--- LemonadeStand.java
+++ LemonadeStand.java
@@ -1,105 +1,90 @@
 import java.util.*;
 public class LemonadeStand {
-	//this is a function to round to .01 to make simpler answers
-	public static double round100(double dble){
-		dble = dble*100;
-		dble = Math.round(dble);
-		return dble/100;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//making scanner
-		Scanner sc = new Scanner(System.in);
-		//making boolean for shutting store true= store is open
-		boolean open = true;
-		double runningtotal = 0;
-		while(open){
-			System.out.println("What is your name?");
-			String name = sc.next();
-			//got name
-			System.out.println("How many brownies did you buy?");
-			String brownies;
-			brownies = sc.next();
-			int brownienum = Integer.parseInt(brownies);
-			//got brownie#s
-			
-			System.out.println("How many glasses of lemonade did you buy?");
-			String lemonade;
-			lemonade = sc.next();
-			int lemonadenum = Integer.parseInt(lemonade);
-			//got lemonade#s
-			boolean discountbrownie = false; 
-			double nbrownie = brownienum;
-			//calculating brownie discount if brownnies + lemonade arent both discounted
-			if(!(brownienum>4&lemonadenum>4))
-				if(brownienum>4){
-					nbrownie = nbrownie*.9; 
-					discountbrownie = true; 
-				}
-			//got brownie discount
-			
-			//if they bought five or more brownies discount 10%
-			//calculating lemonade discount if brownies + lemonade arent both discounted 
-			boolean discountlem = false;
-			double nlemonade = lemonadenum;
-			if(!(brownienum>4&lemonadenum>4))
-				if(lemonadenum>4){
-					nlemonade = nlemonade*.95;
-					discountlem = true; 
-				}
-			//got lemonade discount
-			//if they got five or more lemonades, discount 10%
-			//getting discount  total if lemonade + brownies are both discounted
-			boolean discounttotal = false; 
-			if((brownienum>4&lemonadenum>4)){
-				nbrownie = nbrownie*.78;
-				nlemonade = nlemonade*.78;
-				discounttotal = true;
-			}
-			//got bigger discount 
-			double cost = nbrownie*1.29 + nlemonade*.79;
-			//getting sales tax before i change the cost 
-			double salestax;
-			salestax = cost*.06;
-			double newcost; 
-			newcost = cost*1.06;
-			//printing out final results 
-			System.out.println(name+", your purchased "+brownienum+" brownies and "+lemonadenum+" lemonades");
-			newcost = round100(newcost);
-			System.out.println("Your total cost is: "+newcost+". Thank you for your business!");
-			runningtotal = runningtotal+newcost;
-			//saying what discounts the customer got
-			if(discountlem){
-				double lemdiscount = lemonadenum*.79*.05;
-				lemdiscount = round100(lemdiscount);
-				System.out.println("You were discounted "+lemdiscount+" because you bought more than 5 lemonades");
-				
-			}
-			
-			if(discountbrownie){
-				double browniediscount = brownienum*1.29*.1;
-				browniediscount = round100(browniediscount);
-				System.out.println("You were discounted "+browniediscount+" because you bought more than 5 lemonades");
-			}
-			
-			if(discounttotal){
-				double totaldiscount = cost*.22;
-				totaldiscount = round100(totaldiscount);
-				System.out.println("You were discounted "+totaldiscount+" because you bought more than 5 lemonades and more than 5 brownies.");
-				
-			}
-			System.out.println("Is the store closing? Please say yes or no.");
-			String answerclose = sc.next();
-			if(answerclose.compareTo("yes")==0)
-				open = false;
-			
-			
-			
-		}
-		System.out.println("The total sales for the day were:"+runningtotal);
 	
+	public static void main(String[] args) {
 		
+		//make variables for if there is a customer and a running total
+		boolean customer=true;
+		double runtot=0;
 		
-	}
+		while(customer){//while there are customers in line
+			
+			//instantiate variables
+			int lems, brs, bd=0, ld=0;
+			double lemcost=.79, bcost=1.29, total=0, totbrs, totlems;
+			String nm;
+			Scanner sc=new Scanner(System.in);
+			
+			 
+			System.out.println("What is their name?");//ask for name
+			nm=sc.next();//store it in nm
+			
+			System.out.println("How many brownies did they buy?");//find num of brownies bought
+			brs=sc.nextInt();//store in brs
+			
+			if(brs>=5){//if they bought more than 5
+				totbrs=brs*bcost*.9;//calculate discounted brownie price
+				bd++;//make it true
+			}//end of if
+			
+			else//didn't buy more than 5 brownies
+				totbrs=brs*bcost;//no discount
+			
+			System.out.println("How many lemonades did they buy?");//find num of lemonades bought
+			lems=sc.nextInt();//store in lems
+			
+			if(lems>=5){//if they bought more than 5
+				totlems=lems*lemcost*.95;//calculate discounted lemonade price
+				ld++;//make discount for lems true
+			}//end of if
+			
+			else //didn't buy more than 5
+				totlems=lems*lemcost; //calculate regular price
+			
+			if (ld==1 && bd==1){ //if they got both discounts
+				total=((totlems/.95)+(totbrs/.9))*.78; //reset prices, then give a 22% discount
+			}//end of if
+			
+			else //if they didn't
+				total+=totlems+totbrs;//calculate regular total
+			
+			
+			
+			//end script
+			System.out.println(nm+" bought "+lems+" lemonades for $"+lems*lemcost+" and "+brs+" brownies for $"+brs*bcost+".");//items bought
+			
+			//if they got big discount
+			if (ld==1 && bd==1){
+				System.out.println("They got a 22% discount on their entire purchase! This brings their total to $"+total+" without tax.");//tell them
+			}//end of if
+			
+			else if (ld==1)//only got lemonade discount
+				System.out.println("They got a 5% discount on lemonades, bringing their total down to $"+total+".");
+			
+			else if (bd==1)//only got brownie discount
+				System.out.println("They got a 10% discount on brownies, bringing their total down to $"+total+".");
+			
+			else//no discount
+				System.out.println("They got no discounts.");
+			
+			total*=1.06;//calculate total with sales tax
+			
+			//tell them about sales tax and their total
+			System.out.println("There was a 6% sales tax, which brings the final price to $"+total+".");
+			
+			//last line from Little Jimmy
+			System.out.println("Thank you for visiting Little Jimmy's Lemonade Emporium!");
+			
+			runtot+=total;//add total to running total
+			System.out.println("We have $"+runtot+" in sales today.");//print running total
+			
+			System.out.println("Is there another customer?");//check for more in line
+			customer=sc.nextBoolean();
+			
+			
+		}	//end of while
+	
+	}//end of main
+	
 
 }
