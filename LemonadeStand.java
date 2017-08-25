dbd0134a60bbd3956658ef4890bf5d47836bb39b
JamesBran <31349572+JamesBran@users.noreply.github.com>
2017-08-25 10:29:40 -0400

    Add files via upload

--- LemonadeStand.java
+++ LemonadeStand.java
@@ -1,79 +1,105 @@
 import java.util.*;
 public class LemonadeStand {
-
+	//this is a function to round to .01 to make simpler answers
+	public static double round100(double dble){
+		dble = dble*100;
+		dble = Math.round(dble);
+		return dble/100;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc= new Scanner(System.in);
-		final double lemCost=.79;
-		final double brownCost=1.29;
-		final double brownDiscount=.9;
-		final double lemDiscount=.95;
-		final double totalDiscount=.78;
-		
-		boolean lastPurchase=false;
-		double totalEarnings=0;
-		
-	while(!lastPurchase){	
-			double totalSavings=0;
-			double totalCost=0;
-			int numBrownies=0;
-			int numLemonades=0;
-			String Name;
-		
-			boolean bDisc=false;
-			boolean lDisc=false;
-			boolean tDisc=false;
-		
-			System.out.println("What's your name?");
-			Name=sc.next();
-			System.out.println("How many glasses of lemonade would you like?");
-			numLemonades=sc.nextInt();
-			System.out.println("How many brownies would you like?");
-			numBrownies=sc.nextInt();
-		
-			totalCost=numBrownies*brownCost + numLemonades*lemCost;
-			if(numBrownies>=5 && numLemonades>=5){
-				totalSavings=totalCost-(totalDiscount*(numBrownies*brownCost + numLemonades*lemCost));
-				totalCost=totalDiscount*(numBrownies*brownCost + numLemonades*lemCost);
-				tDisc=true;
-			}else if(numBrownies>=5 && numLemonades<5){
-				totalCost=numBrownies*brownCost*brownDiscount + numLemonades*lemCost;
-				bDisc=true;
-				totalSavings=numBrownies*brownCost*(1-brownDiscount);
-			}else if(numBrownies<5 && numLemonades>=5){
-				totalCost=numBrownies*brownCost + numLemonades*lemCost*lemDiscount;
-				lDisc=true;
-				totalSavings=numLemonades*lemCost*(1-lemDiscount);
-			}
-			totalEarnings+=totalCost;
-			totalCost=totalCost*1.06; //taxes
+		//making scanner
+		Scanner sc = new Scanner(System.in);
+		//making boolean for shutting store true= store is open
+		boolean open = true;
+		double runningtotal = 0;
+		while(open){
+			System.out.println("What is your name?");
+			String name = sc.next();
+			//got name
+			System.out.println("How many brownies did you buy?");
+			String brownies;
+			brownies = sc.next();
+			int brownienum = Integer.parseInt(brownies);
+			//got brownie#s
 			
-			System.out.println("Customer Name: "+Name);
-			System.out.println("Your Purchases:");
-			if(numBrownies>0){
-				System.out.println(numBrownies+" Brownies........$"+brownCost*numBrownies);
-				if(bDisc){
-					System.out.println("You received a 10% discount on your brownies!");
+			System.out.println("How many glasses of lemonade did you buy?");
+			String lemonade;
+			lemonade = sc.next();
+			int lemonadenum = Integer.parseInt(lemonade);
+			//got lemonade#s
+			boolean discountbrownie = false; 
+			double nbrownie = brownienum;
+			//calculating brownie discount if brownnies + lemonade arent both discounted
+			if(!(brownienum>4&lemonadenum>4))
+				if(brownienum>4){
+					nbrownie = nbrownie*.9; 
+					discountbrownie = true; 
 				}
-			}
-			if(numLemonades>0){
-				System.out.println(numLemonades+" Lemonades.......$"+lemCost*numLemonades);
-				if(lDisc){
-					System.out.println("You received a 5% discount on your lemonade!");
+			//got brownie discount
+			
+			//if they bought five or more brownies discount 10%
+			//calculating lemonade discount if brownies + lemonade arent both discounted 
+			boolean discountlem = false;
+			double nlemonade = lemonadenum;
+			if(!(brownienum>4&lemonadenum>4))
+				if(lemonadenum>4){
+					nlemonade = nlemonade*.95;
+					discountlem = true; 
 				}
+			//got lemonade discount
+			//if they got five or more lemonades, discount 10%
+			//getting discount  total if lemonade + brownies are both discounted
+			boolean discounttotal = false; 
+			if((brownienum>4&lemonadenum>4)){
+				nbrownie = nbrownie*.78;
+				nlemonade = nlemonade*.78;
+				discounttotal = true;
 			}
-			if(tDisc){
-				System.out.println("You received a 22% discount on your entire purchase!");
+			//got bigger discount 
+			double cost = nbrownie*1.29 + nlemonade*.79;
+			//getting sales tax before i change the cost 
+			double salestax;
+			salestax = cost*.06;
+			double newcost; 
+			newcost = cost*1.06;
+			//printing out final results 
+			System.out.println(name+", your purchased "+brownienum+" brownies and "+lemonadenum+" lemonades");
+			newcost = round100(newcost);
+			System.out.println("Your total cost is: "+newcost+". Thank you for your business!");
+			runningtotal = runningtotal+newcost;
+			//saying what discounts the customer got
+			if(discountlem){
+				double lemdiscount = lemonadenum*.79*.05;
+				lemdiscount = round100(lemdiscount);
+				System.out.println("You were discounted "+lemdiscount+" because you bought more than 5 lemonades");
+				
 			}
-			System.out.println("Total Cost: $"+totalCost);
-			System.out.println("Total Savings: $"+ totalSavings);
-			System.out.println("Have a Finky Day!");
-			System.out.println("End?");
-			int thing=sc.nextInt();
-			if(thing==1){
-				lastPurchase=true;
+			
+			if(discountbrownie){
+				double browniediscount = brownienum*1.29*.1;
+				browniediscount = round100(browniediscount);
+				System.out.println("You were discounted "+browniediscount+" because you bought more than 5 lemonades");
 			}
+			
+			if(discounttotal){
+				double totaldiscount = cost*.22;
+				totaldiscount = round100(totaldiscount);
+				System.out.println("You were discounted "+totaldiscount+" because you bought more than 5 lemonades and more than 5 brownies.");
+				
+			}
+			System.out.println("Is the store closing? Please say yes or no.");
+			String answerclose = sc.next();
+			if(answerclose.compareTo("yes")==0)
+				open = false;
+			
+			
+			
 		}
-		System.out.println("Total Earnings Today: $"+totalEarnings);
+		System.out.println("The total sales for the day were:"+runningtotal);
+	
+		
+		
 	}
+
 }
